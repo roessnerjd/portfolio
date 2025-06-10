94d9fbb413c5499cd0ab4bc8c0c07cf923e5556e
roessnerjd <roessner_jacob@yahoo.com>
2025-06-09 19:10:27 -0700

    added text effect for h1 heading

--- portfolio_jacobr.js
+++ portfolio_jacobr.js
@@ -1,3 +1,4 @@
+// creates a smooth effect when going to section clicked for on navbar
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -7,7 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// Inform user of submit
+// pop up box when message is sent
 const contactForm = document.querySelector("#contact form");
 if (contactForm) {
   contactForm.addEventListener("submit", function (e) {
@@ -16,3 +17,19 @@ if (contactForm) {
     this.reset();
   });
 }
+
+// typewriter effect, pulled from: http://w3schools.com/howto/howto_js_typewriter.asp
+const text = "Hello, I am Jacob Roessner.";
+let i = 0;
+const speed = 50;
+
+function typeWriter() {
+  const target = document.querySelector(".typewriter");
+  if (i < text.length) {
+    target.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.addEventListener("load", typeWriter);
